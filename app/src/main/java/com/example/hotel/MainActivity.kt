86713e99e7a305a3144c317891ec7642f285f11f
AnvarZamirov <zamirovanvar26@gmail.com>
2023-12-07 18:41:56 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hotel/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hotel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onClickGoMian(view : View) {
+        val intent = Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+}
